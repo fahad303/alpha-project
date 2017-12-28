6b018f8980a338b4b4a1d57ad62ebc1e184ef7cb
rails course <fadesheikh0@gmail.com>
2017-12-28 12:50:28 +0000

    final

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
    
+  # before_action :set_article, only: [:edit, :update, :show, :destroy] (for removing this same line @article = Article.find(params[:id]) using many times in which functions )
+   
    def index
       @articles = Article.all 
    end
