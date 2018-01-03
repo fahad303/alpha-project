b23937699214d860e751cc69f2a1d158298bebe8
rails course <fadesheikh0@gmail.com>
2018-01-03 11:49:18 +0000

    user create read  update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,24 @@ class UsersController < ApplicationController
       end
    end
    
+   def edit
+   @user = User.find(params[:id])
+   end
+   
+   def update
+   @user = User.find(params[:id])
+   if @user.update(user_params)
+      flash[:success] = "Your Info has been successfully updated"
+      redirect_to user_path
+   else
+      render 'edit'
+   end
+   end
+   
+   def show
+   @user = User.find(params[:id])
+   end
+   
    private
    def user_params
       params.require(:user).permit(:username, :email, :password)
