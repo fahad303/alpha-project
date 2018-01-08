9b2db1476fe31af47f631edc952ee51f820926c0
rails course <fadesheikh0@gmail.com>
2018-01-08 07:31:24 +0000

    complete authenticating system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+   before_action :require_same_user, only: [:edit, :update] 
    
    def index
       @users = User.paginate(page: params[:page], per_page: 5)
@@ -36,6 +37,13 @@ class UsersController < ApplicationController
    @user = User.find(params[:id])
    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
    end
+   
+   def require_same_user
+      if current_user != @user
+         flash[:danger] = "You Have Been Reaching Wrong Account"
+         redirect_to root_path
+      end
+   end 
    
    private
    def user_params
