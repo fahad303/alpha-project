3c7a256f98d36e35d16bc471de5ffeb640c3412c
rails course <fadesheikh0@gmail.com>
2018-01-10 12:09:31 +0000

    setting up categories listing in navigation bar

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
     
+    before_action :require_admin, except: [:index, :show]
+    
     def index
         @categories = Category.paginate(page: params[:page], per_page: 4)
     end
@@ -20,6 +22,13 @@ class CategoriesController < ApplicationController
     
     def show
         
+    end
+    
+    def require_admin
+       if !logged_in? || (logged_in? and !current_user.admin?)
+          flash[:danger] = "Only admin perform that action"
+          redirect_to categories_path
+       end
     end
     
     private
