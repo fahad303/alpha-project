7686a2ddfcf4eef452c05ddb63412d6dbe5dd0fa
rails course <fadesheikh0@gmail.com>
2018-02-08 11:36:53 +0000

    setting passward confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
     has_many :articles, dependent: :destroy
     before_save { self.email = email.downcase }
+    
+    attr_accessor :password_confirmation
+    
     validates :username, presence: true, 
                uniqueness: { case_sensitive: false },
                length: {minimum: 4, maximum: 24}
